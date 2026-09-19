cad31ce0413e4d8ecc2d0adf5f1f107cb0719387
Abrar Amin <abrarabrar4766@gmail.com>
2026-09-20 02:50:23 +0600

    Allow cors

--- api/atcoder.js
+++ api/atcoder.js
@@ -1,21 +1,33 @@
 import { fetchUserInfo } from '@qatadaazzeh/atcoder-api';
 
-export default function handler(req, res) {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        '*'
-    );
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, OPTIONS'
     );
 
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type'
+    );
+
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
 
-    return res.status(200).json({
-        message: 'API is working'
-    });
+    try {
+        const user = await fetchUserInfo('abra_r');
+
+        return res.status(200).json({
+            userMaxRating: user.userMaxRating
+        });
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            error: 'Failed to fetch AtCoder information'
+        });
+    }
 }
