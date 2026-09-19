6ae4ef8f3f6638fe1c5f47e11d1a76b28b6c3868
Abrar Amin <abrarabrar4766@gmail.com>
2026-09-20 02:40:07 +0600

    Test Vercel API

--- api/atcoder.js
+++ api/atcoder.js
@@ -1,43 +1,21 @@
 import { fetchUserInfo } from '@qatadaazzeh/atcoder-api';
 
-export default async function handler(req, res) {
-    const allowedOrigins = [
-        'http://localhost:5173',
-        'https://abra-r.github.io'
-    ];
-
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+export default function handler(req, res) {
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        '*'
+    );
 
     res.setHeader(
         'Access-Control-Allow-Methods',
         'GET, OPTIONS'
     );
 
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type'
-    );
-
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
 
-    try {
-        const user = await fetchUserInfo('abra_r');
-
-        return res.status(200).json({
-            userMaxRating: user.userMaxRating
-        });
-
-    } catch (error) {
-        console.error(error);
-
-        return res.status(500).json({
-            error: 'Failed to fetch AtCoder information'
-        });
-    }
+    return res.status(200).json({
+        message: 'API is working'
+    });
 }
